58bb5ef1106bb366c38b410e8a61ce7bc6aacad0
mrislam15 <mdrashedul.islam@qcc.cuny.edu>
2025-01-02 15:15:13 -0500

    Add files via upload

---
+++ testing.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    string username ="";
+    cout<<"What is your full name? ";
+    cin>>username;
+    cout<<"Welcome to C++ programming "<<username<<endl;
+    return 0;
+}

396f91b109079f27af2d77d4817c735cef0df4d5
mrislam15 <mdrashedul.islam@qcc.cuny.edu>
2025-01-10 15:45:15 -0500

    Add files via upload

---
+++ Lab10_Rashedul_function.cpp
@@ -0,0 +1,26 @@
+/*
+MD Rashedul Islam
+Lab 10, Function
+*/
+
+
+#include<iostream>
+using namespace std;
+//example 1:void function
+//print 'Hello Function!'
+
+void printHi(){
+    cout<<"Hello Function! "<<endl;
+}
+
+//example 2: Void function with parameters
+void greeting(string name){
+    cout<<"Welcome to c++ "<<name<<endl;
+}
+
+
+
+//example 3: 
+int numbertriple(int number){
+    return number*3;
+}
